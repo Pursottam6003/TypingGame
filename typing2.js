eb3fb8cf0d5f1e34feff6a80bd6bb0f11104cf0d
Pursottam Sah <81693090+Pursottam6003@users.noreply.github.com>
2022-01-14 15:58:49 +0530

    Update typing2.js
    modify comments

--- typing2.js
+++ typing2.js
@@ -1,30 +1,41 @@
-
-// The paragraph that we will use to write as reference for typing  
-let firstInput=false;
-//let quote="The perennial feature in many a high school syllabus, Shirley Jackson's best-known short story clinically details an unusual ritual that takes place in a small town."
-
-// separating the words from a big string into spaces and storing into array of string  
-//word=quote.split(' ');
-//creating the variable for storing the values of initial point in seconds
+// The Java Script code for the Typing Game
+// Selecting the  start button so that we will show the paragraph in the display block 
+btn = document.getElementById('start');
+// Selecting the input box from id of input
+input = document.querySelector('input');
+// selecting the article tag 
+article = document.querySelector('article');
+// Selecting the id to display the final message
+greet=document.getElementById('final_message');
+// message after the writing part is over to diplay the word
+message=document.getElementById('message');
+// Time taken by the user in writing so this need to displayed here by selecting 
+time_taken = document.getElementById('duration');
+// variables
+let firstInput=false; //for first input
 let words,quote;
 let first=0,last=0;
+let word_index = 0,correct_word=0,incorrect_word=0;
 
-//trying to select the element by id and assigning into btn variable
-btn = document.getElementById('start');
-
+ 
 //creating a function that waits for start click 
 // in this function we will try to append into a string from array of string and printing into the console
+// creating a function on clicking input the function will execute
+// in this function it is chcking that the words written from input box matches with word[i] or not
+// if matches then it is highlighted with yellow colour otherwise it is red  colour
+// creating the object
+
 btn.addEventListener('click',function(){
-    article = document.querySelector('article');
+   
     html=''
     div =document.getElementById('block');
     div.style.display='block';
    
-    $.get('https://api.quotable.io/random?tags=technology|health|civil-rights,famous-quotes', function(response){
+    $.get('https://api.quotable.io/random?minLength=100&maxLength=160', function(response){
 
-        quote=response['content'];
-        words=quote.split(' ');
-        let html=''; 
+        quote=response['content']; // storing into quote variable
+        words=quote.split(' '); //spliting it into string 
+        let html=''; //a string that will store the whole words
 
         for(let i=0;i<words.length;i++)
         {  
@@ -33,46 +44,31 @@ btn.addEventListener('click',function(){
 
         for(let i=0;i<words.length;i++)
         {
-            words[i]=words[i]+' ';
+            words[i]=words[i]+' '; // adding a space in each word 
         }
-        article=document.querySelector('article');
-
-        console.log(html);  
-        article.innerHTML =html;
+        console.log(html);  //checking purpose
+        article.innerHTML =html;// assigning into the article tag and with the help of this will execute
     });
    
 }) 
 
-//selecting the input box from id of input
-input = document.querySelector('input');
-
-// initialising the variable 
-let word_index = 0,correct_word=0,incorrect_word=0,previous_word=-1;
-// creating a function on clicking keyup the function will execute
-// in this function it is chcking that the words written from input box matches with word[i] or not
-// if matches then it is highlighted with yellow colour otherwise it is red  colour
-// creating the object
 
 input.addEventListener('input',function(e){
     if(firstInput==false)
-    {
-        const StartingTime= new Date();
+    {   input.value=''; //emptying the input box after the start click 
+        const StartingTime= new Date(); //checking the date
         first=StartingTime.getTime();
         firstInput=true;
     }
-    
-    console.log(first);
-    console.log("hello");
-   
-    
-    if(input.value==words[word_index].slice(0,input.value.length))
+
+    if(input.value==words[word_index].slice(0,input.value.length)) // checking character by character
     {   
         if((input.value==words[word_index]))
-        {   console.log('hola');
+        { 
             document.getElementById('w'+word_index).style.backgroundColor='transparent';
             if(word_index+1<words.length)
             {
-                document.getElementById('w'+(word_index+1)).style.backgroundColor='yellow'; 
+                document.getElementById('w'+(word_index+1)).style.backgroundColor='yellow'; //highlighting
             }
             word_index++;
             input.value=''; // after it will make the text box empty
@@ -80,34 +76,26 @@ input.addEventListener('input',function(e){
             console.log(word_index);
             correct_word++;
         }
-        input.style.backgroundColor = 'white';
+        input.style.backgroundColor = 'white'; //removing form highlited and changing into original color of div
         input.style.color='black';
         
-
         if(word_index==words.length)
         {   const EndingTime = new Date(); //storing the final time 
             end=EndingTime.getTime();
             message.style.display='block';
-            greet=document.getElementById('final_message');
             greet.innerHTML="Congratulations you have sucessfully completed the task !";
-            message=document.getElementById('message');
+           
             // calculating the final time
             duration=end-first;
-            console.log(duration);
+            console.log(duration); //checking
             // converting into seconds
 
-
             var Totalseconds=((duration/1000));
-            var seconds= Totalseconds %60;
+            var seconds= Math.floor(Totalseconds %60);
             var minutes= Math.floor(Totalseconds/60);
-     
-          
-  
-            time_taken = document.getElementById('duration');
-            time_taken.innerHTML="Time Taken "+ minutes+":"+seconds;
+       
 
-            console.log(minutes);
-            console.log(seconds);
+            time_taken.innerHTML="Time Taken "+ minutes+":"+seconds;
             
             accuracy=document.getElementById('Accuracy');
             var aggregade=(((quote.length-incorrect_word)/quote.length)*100).toFixed(2);
@@ -124,6 +112,10 @@ input.addEventListener('input',function(e){
     }
     
 });
+
+
+// The end  if you want any changes please contribute
+
 
 
 
