e1186011b5df498579111aba3a9ba1b77f620a89
Pursottam Sah <rahulsah6003@gmail.com>
2022-01-14 12:50:50 +0530

    improve time calculation

--- typing2.js
+++ typing2.js
@@ -1,14 +1,14 @@
 
 // The paragraph that we will use to write as reference for typing  
-
-let quote="The perennial feature in many a high school syllabus, Shirley Jackson's best-known short story clinically details an unusual ritual that takes place in a small town."
+let firstInput=false;
+//let quote="The perennial feature in many a high school syllabus, Shirley Jackson's best-known short story clinically details an unusual ritual that takes place in a small town."
 
 // separating the words from a big string into spaces and storing into array of string  
-word=quote.split(' ');
+//word=quote.split(' ');
 //creating the variable for storing the values of initial point in seconds
-let first,last;
-// creating the object
-const StartingTime= new Date();
+let words,quote;
+let first=0,last=0;
+
 //trying to select the element by id and assigning into btn variable
 btn = document.getElementById('start');
 
@@ -20,13 +20,26 @@ btn.addEventListener('click',function(){
     div =document.getElementById('block');
     div.style.display='block';
    
-    // appending into span so that we will try to highlight the words 
-    for (let i=0;i<word.length;i++)
-    {
-        html +="<span id='w"+i+"'>"+word[i]+' '+"</span>"; 
-    }
-    console.log(html);
-    article.innerHTML= html;
+    $.get('https://api.quotable.io/random?tags=technology,famous-quotes', function(response){
+
+        quote=response['content'];
+        words=quote.split(' ');
+        let html=''; 
+
+        for(let i=0;i<words.length;i++)
+        {  
+            html +="<span id='w"+i+"'>"+words[i]+' '+"</span>"; 
+        }
+
+        for(let i=0;i<words.length;i++)
+        {
+            words[i]=words[i]+' ';
+        }
+        article=document.querySelector('article');
+
+        console.log(html);  
+        article.innerHTML =html;
+    });
    
 }) 
 
@@ -38,15 +51,26 @@ let word_index = 0,correct_word=0,incorrect_word=0,previous_word=-1;
 // creating a function on clicking keyup the function will execute
 // in this function it is chcking that the words written from input box matches with word[i] or not
 // if matches then it is highlighted with yellow colour otherwise it is red  colour
-input.addEventListener('keyup',function(e){
-    first=StartingTime.getTime();
+// creating the object
+
+input.addEventListener('input',function(e){
+    if(firstInput==false)
+    {
+        const StartingTime= new Date();
+        first=StartingTime.getTime();
+        firstInput=true;
+    }
+    
+    console.log(first);
     console.log("hello");
-    if(input.value==word[word_index].slice(0,input.value.length))
+   
+    
+    if(input.value==words[word_index].slice(0,input.value.length))
     {   
-        if(input.value.length==word[word_index].length)
-        {     
+        if((input.value==words[word_index]))
+        {   console.log('hola');
             document.getElementById('w'+word_index).style.backgroundColor='transparent';
-            if(word_index+1<word.length)
+            if(word_index+1<words.length)
             {
                 document.getElementById('w'+(word_index+1)).style.backgroundColor='yellow'; 
             }
@@ -60,7 +84,7 @@ input.addEventListener('keyup',function(e){
         input.style.color='black';
         
 
-        if(word_index==word.length)
+        if(word_index==words.length)
         {   const EndingTime = new Date(); //storing the final time 
             end=EndingTime.getTime();
             message.style.display='block';
@@ -69,15 +93,24 @@ input.addEventListener('keyup',function(e){
             message=document.getElementById('message');
             // calculating the final time
             duration=end-first;
+            console.log(duration);
             // converting into seconds
-            
-            let minutes=Math.floor(duration/60000);
-            let seconds=((duration %60000)/1000).toFixed(0);
+
+
+            var Totalseconds=((duration/1000));
+            var seconds= Totalseconds %60;
+            var minutes= Math.floor(Totalseconds/60);
+     
+          
+  
             time_taken = document.getElementById('duration');
             time_taken.innerHTML="Time Taken "+ minutes+":"+seconds;
+
+            console.log(minutes);
+            console.log(seconds);
             
             accuracy=document.getElementById('Accuracy');
-            let aggregade=(((quote.length-incorrect_word)/quote.length)*100).toFixed(2);
+            var aggregade=(((quote.length-incorrect_word)/quote.length)*100).toFixed(2);
             accuracy.innerHTML="Accuracy: "+aggregade;
         }
 
@@ -85,7 +118,7 @@ input.addEventListener('keyup',function(e){
 
     else 
     {   // changing the background of input box as red as it is not matched with consecutive 
-        incorrect_word++;
+        incorrect_word++; 
         input.style.backgroundColor = 'red';
         input.style.color='white';
     }
